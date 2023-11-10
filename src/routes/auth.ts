75fbcf360839896e4a33979953a0dff608a945f3
a-pillow <kaluhblee@gmail.com>
2023-11-09 19:43:09 -0600

    increase rate limit

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import { loginSchema, registerSchema } from "../validations";
-import { login, logout, me, register, withSession } from "../controllers/auth.controller";
+import {
+  login,
+  logout,
+  me,
+  register,
+  withSession,
+} from "../controllers/auth.controller";
 import { rateLimit } from "express-rate-limit";
 import { logger } from "../server";
 
@@ -8,24 +14,32 @@ const router = express();
 
 //Logger
 router.use((req: Request, res: Response, next: NextFunction) => {
-    logger.log("info", `[auth] received ${req.method} request for ${req.url} from ${req.hostname}`)
+  logger.log(
+    "info",
+    `[auth] received ${req.method} request for ${req.url} from ${req.hostname}`
+  );
 
-    res.on("finish", () => {
-        logger.log("info", `[auth] finished ${req.method} request for ${req.url}: ${res.statusCode} ${res.statusMessage}`)
-    })
+  res.on("finish", () => {
+    logger.log(
+      "info",
+      `[auth] finished ${req.method} request for ${req.url}: ${res.statusCode} ${res.statusMessage}`
+    );
+  });
 
-    next();
-})
+  next();
+});
 
-router.get("/me", withSession, me)
-router.use(rateLimit({
+router.get("/me", withSession, me);
+router.use(
+  rateLimit({
     windowMs: 1 * 60 * 1000,
-    max: 20, //20 requests per minute
+    max: 60, //20 requests per minute
     standardHeaders: "draft-7",
-    legacyHeaders: false
-}))
-router.post("/login", [...loginSchema], login)
-router.post("/register", [...registerSchema], register)
-router.post("/logout", withSession, logout)
+    legacyHeaders: false,
+  })
+);
+router.post("/login", [...loginSchema], login);
+router.post("/register", [...registerSchema], register);
+router.post("/logout", withSession, logout);
 
 export { router as AuthRouter };
